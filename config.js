22daa30825c6e8499be545361adbb822bdddc182
lout33 <lout33@users.noreply.github.com>
2026-01-21 09:09:51 +0100

    Update Apps Script URL

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // URL de l'API Google Apps Script
-    API_URL: 'https://script.google.com/macros/s/AKfycbxtu0XVUG_s5Gy6Hcqrd-IYg5EVUSFWqj_24OuxqhRczCrQWbXaqQE7NC8XbZ4-5B5X/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbwUY3QaykEi8f0oe70lmSG_08yYgjS4XNyP8dtttQFXlnfLLrUCS6SXLa1TEpK-Fu7Q/exec',
 
     // Types de massages disponibles (modifiable selon l'offre)
     MASSAGE_TYPES: [
